23c12b9994ef404a8ca8d1b6835a0e3f9b8f6016
NicolaMacis <117011283+NicolaMacis@users.noreply.github.com>
2022-12-20 10:22:09 +0100

    Add java part

--- Angular/src/app/@core/services/auth.service.ts
+++ Angular/src/app/@core/services/auth.service.ts
@@ -2,27 +2,25 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { of } from "rxjs";
+import { LoginComponent } from "src/app/@shared/components/login/login.component";
 import { LoginDTO, RegisterDTO, User } from "src/app/models/user";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  springBootUrl = 'http://localhost:8080';
+  springBootUrl = 'http://localhost:8080/';
 
   constructor(private router: Router, private http: HttpClient) {}
 
   login(loginData: LoginDTO) {
     console.log('auth service.ts', loginData);
+    return this.http.get(`${this.springBootUrl + 'users/login/'}${loginData.username}/${loginData.password}`);
 
-    // Passare username e password
-    return this.http.get(`${this.springBootUrl}/api/user`);
   }
-
   register(registerData: RegisterDTO) {
-    // TODO Chiamare il servizio per la registrazione e redirigere l'utente alla root per il login
-    
-    this.router.navigateByUrl("/");
+    this.router.navigateByUrl("/")
+    return this.http.post<RegisterDTO>(`${this.springBootUrl + 'utility/registrazione'}`, registerData);
   }
 
   logout() {
